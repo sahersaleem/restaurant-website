31ab7856a0b2831d485792551c6e4e2e123d62a0
Muhammad Saad <160317082+sahersaleem@users.noreply.github.com>
2025-05-04 15:47:14 +0500

    more changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,25 +4,24 @@ import All_restaurants from "@/components/landingPageComponent/All_restaurants";
 import Footer from "@/components/landingPageComponent/Footer";
 import Hero from "@/components/landingPageComponent/Hero";
 import Navbar from "@/components/landingPageComponent/Navbar";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div className=" w-full lg:bg-[#18171C] h-[90vh]  ">
-         <Navbar />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Navbar />
+      </Suspense>
+      <Hero />
+      <div className="h-auto max-w-7xl mx-auto mb-20 sm:mb-0  ">
+      <Suspense fallback={<div>Loading...</div>}>
 
-    
-    
-   
-     
-        <Hero />
-        <div className="h-auto max-w-7xl mx-auto mb-20 sm:mb-0  ">
-        <All_restaurants/>
-   
+        <All_restaurants />
+        </Suspense>
       </div>
-      
-      <Advertisement position="inline"/>
-      <Footer/>
- 
+
+      <Advertisement position="inline" />
+      <Footer />
     </div>
   );
 }
